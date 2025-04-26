cd6df757cce59c7d16c21af6c1d3b5d3e6513c87
safeedha <mksafeedha@gmail.com>
2025-04-26 15:48:19 +0530

    mrn chat application

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ dotenv.config();
 const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
+console.log(process.env.PORT,process.env.MONGO_DB_URI)
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
